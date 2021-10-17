587f5ae5d9887fd095ede6b6606675bc6740a055
daniyoko <dtokimatu@hotmail.com>
2021-10-17 20:00:56 -0300

    masster

---
+++ exercicio12.py
@@ -0,0 +1,6 @@
+from os import system
+system('cls')
+for i in range (1,11):
+    print('{: >3} {: >3} {: >3} {: >3} {: >3} {: >3} {: >3} {: >3} {: >3} {: >3} '.
+    format(i,i*2,i*3,i*4,i*5,i*6,i*7,i*8,i*9,i*10))
+
---
+++ exercicio13.py
@@ -0,0 +1,12 @@
+numero = 5
+contador = 0
+i = 0
+while i <= numero or contador < 2:
+    i = i + 1
+    x = numero % i
+    if x == 0:
+            contador = contador +1
+if contador <= 2:
+    print("primo")
+else:
+    print("não é primo")
